81c91229cba625f6583780ad0e414ce4d8742012
the-vishal <mail007tovishal@gmail.com>
2022-04-23 19:07:49 +0530

    formatted models

--- backend/cart/models.py
+++ backend/cart/models.py
@@ -9,26 +9,28 @@ from rest_framework.authtoken.models import Token
 @receiver(models.signals.post_save, sender=User)
 def user_created(sender, instance, created, **kwargs):
     if created:
-        Token.objects.create(user=user)
+        Token.objects.create(user=instance)
 
 
 class Product(models.Model):
-	id =  models.PositiveIntegerField(unique=True, primary_key=True)
-	name = models.CharField(null=True, blank=True, max_length=20)
-	price = models.FloatField(default=0.0)
-	description = models.CharField(null=True, blank=True, max_length=500)
-	category = models.CharField(null=True, blank=True, max_length=200)
-	image = models.CharField(null=True, blank=True, max_length=500)
-	discount_percentage = models.FloatField(default=None)
-	weight_in_grams = models.FloatField(default=0.0)
+    id = models.PositiveIntegerField(unique=True, primary_key=True)
+    name = models.CharField(null=True, blank=True, max_length=20)
+    price = models.FloatField(default=0.0)
+    description = models.CharField(null=True, blank=True, max_length=500)
+    category = models.CharField(null=True, blank=True, max_length=200)
+    image = models.CharField(null=True, blank=True, max_length=500)
+    discount_percentage = models.FloatField(default=0.0)
+    weight_in_grams = models.FloatField(default=0.0)
+
 
 class CartItem(models.Model):
-	product = models.ForeignKey(Product, on_delete=models.CASCADE)
-	quantity = models.PositiveIntegerField(default=0)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
+
 
 class Cart(models.Model):
-	user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
-	items = models.ManyToManyField(CartItem)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    items = models.ManyToManyField(CartItem)
 
-	def get_checkout_value():
-		pass
+    def get_checkout_value():
+        pass
